d659f95f94e7c1d7a41b891bf3f9489fe6051ab9
Diego Reyes <diegorele13@gmail.com>
2023-02-27 17:59:45 -0500

    learn about strings

---
+++ src/es6/03-strings.js
@@ -0,0 +1,17 @@
+let hello = "Hello";
+let world = "world";
+
+let epicPhrase = hello + " " + world + "!";
+
+// template literals
+let epcPhrase2 = `${hello} ${world}!`;
+console.log(epcPhrase2);
+
+// Multi-line strings
+let lorem = "Esto es un string \n" + "esto es otra linea";
+
+let lorem2 = `Esta es un frase epica
+   la continuacion de esa fase epica
+`;
+console.log(lorem);
+console.log(lorem2);

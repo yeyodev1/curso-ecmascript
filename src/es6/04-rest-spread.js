31de76a1f9d11f2474ba05d7171ca8ad0e16a104
Diego Reyes <diegorele13@gmail.com>
2023-02-28 12:20:47 -0500

    learn about destructuring

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,10 @@
+//arrays destructuring
+let fruits = ["Apple", "Banana"];
+let [a, b] = fruits;
+console.log(a, fruits[1]);
+
+//Object destructuring
+
+let user = { username: "Diego", age: 17 };
+let { username, age } = user;
+console.log(username, age);

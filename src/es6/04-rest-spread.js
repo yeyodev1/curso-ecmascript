3c405544d94d4b84cb28b98bb2ed74b942338f43
Diego Reyes <diegorele13@gmail.com>
2023-02-28 12:26:22 -0500

    learn about spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -8,3 +8,18 @@ console.log(a, fruits[1]);
 let user = { username: "Diego", age: 17 };
 let { username, age } = user;
 console.log(username, age);
+
+// Spread operator
+let person = { name: "oscar", age: 28 };
+let country = "Ecuador";
+let data = { ...person, country };
+console.log(data);
+
+// rest
+function sum(num, ...values) {
+	console.log(values);
+	console.log(num + values[0]);
+	return num + values[0];
+}
+
+sum(1, 1, 2, 3);
